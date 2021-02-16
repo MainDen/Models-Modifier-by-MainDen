e49489c9dbd47be3082443d037ff5f49c37101f0
MainDen <MainDen@DESKTOP-MAIN13>
2021-02-16 22:36:23 +0300

    Reviewed and changed modifier system with matches the MVVM pattern.
    Changes:
    Updated all Modifier classes to a new look.
    Removed Model classes.
    
    New:
    Added some styles.
    Added binding to Modifiers.
    Added search bar.
    Added a command for the Apply button.
    
    Refactoring:
    Deleted some needless references.

--- Models Modifier by MainDen/ViewModels/MainWindowViewModel.cs
+++ Models Modifier by MainDen/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using Models_Modifier_by_MainDen.Commands;
+using Modifiers_by_MainDen.Modifiers;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 
 namespace Models_Modifier_by_MainDen.ViewModels
 {
     public class MainWindowViewModel : WindowViewModel
     {
+        static MainWindowViewModel()
+        {
+            InitializeModifiers();
+        }
+
         public MainWindowViewModel(System.Windows.Window window) : base(window)
         {
+            InitializeAppliedModifiers();
         }
 
         private string filePath;
@@ -28,6 +37,126 @@ namespace Models_Modifier_by_MainDen.ViewModels
             get
             {
                 return "Execution time: " + executionTime.TotalSeconds + "s";
+            }
+        }
+
+        private static void InitializeModifiers()
+        {
+            try
+            {
+                modifiers = new ObservableCollection<AbstractModifier>();
+                Type aType = typeof(AbstractModifier);
+                IEnumerable<Type> types = Assembly.GetAssembly(aType).GetTypes().Where(type => type.IsSubclassOf(aType));
+                foreach (var type in types)
+                    modifiers.Add((AbstractModifier)type.GetConstructor(Type.EmptyTypes).Invoke(null));
+            }
+            catch { }
+        }
+        public static ObservableCollection<AbstractModifier> modifiers;
+        public ObservableCollection<AbstractModifier> Modifiers
+        {
+            get
+            {
+                ObservableCollection<AbstractModifier> visibleModifiers = new ObservableCollection<AbstractModifier>();
+                Type type = ResultType;
+                foreach (var modifier in modifiers)
+                    if (modifier.CanBeAppliedTo(type) && IsMatchesTheSearchQuery(SearchText, modifier.Name))
+                        visibleModifiers.Add(modifier);
+                return visibleModifiers;
+            }
+        }
+        private AbstractModifier selectedModifier;
+        public AbstractModifier SelectedModifier
+        {
+            get
+            {
+                return selectedModifier;
+            }
+            set
+            {
+                selectedModifier = value;
+                OnPropertyChanged(nameof(SelectedModifier));
+            }
+        }
+
+        private void InitializeAppliedModifiers()
+        {
+            AppliedModifiers = new ObservableCollection<AbstractModifier>();
+        }
+        public ObservableCollection<AbstractModifier> AppliedModifiers { get; set; }
+        private AbstractModifier selectedAppliedModifier;
+        public AbstractModifier SelectedAppliedModifier
+        {
+            get
+            {
+                return selectedAppliedModifier;
+            }
+            set
+            {
+                selectedAppliedModifier = value;
+                OnPropertyChanged(nameof(SelectedAppliedModifier));
+            }
+        }
+        private Type ResultType
+        {
+            get
+            {
+                Type type = null;
+                foreach (var modifier in AppliedModifiers)
+                    type = modifier.ResultType(type);
+                return type;
+            }
+        }
+        private string searchText = "";
+        public string SearchText
+        {
+            get
+            {
+                return searchText;
+            }
+            set
+            {
+                searchText = value;
+                OnPropertyChanged(nameof(SearchText));
+                OnPropertyChanged(nameof(Modifiers));
+            }
+        }
+        private static bool IsMatchesTheSearchQuery(string query, string str)
+        {
+            if (query is null)
+                throw new ArgumentNullException(nameof(query));
+            if (str is null)
+                throw new ArgumentNullException(nameof(str));
+            if (query == string.Empty)
+                return true;
+            string qStr = query.ToUpper();
+            string sStr = str.ToUpper();
+            int qi = 0;
+            int si = 0;
+            int qLength = qStr.Length;
+            int sLength = sStr.Length;
+            for (; qi < qLength && si < sLength; ++si)
+                if (qStr[qi] == sStr[si])
+                    ++qi;
+            return qi == qLength;
+        }
+        
+        private RelayCommand applyCommand;
+        public RelayCommand ApplyCommand
+        {
+            get
+            {
+                return applyCommand ??
+                    (applyCommand = new RelayCommand(obj =>
+                    {
+                        if (selectedModifier != null)
+                        {
+                            AppliedModifiers.Add(SelectedModifier);
+                            SelectedModifier = null;
+                            SearchText = "";
+                            OnPropertyChanged(nameof(Modifiers));
+                        }
+                    }));
             }
         }
     }
---
+++ Modifiers by MainDen/Modifiers/ImageFromFileModifier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+namespace Modifiers_by_MainDen.Modifiers
+{
+    public class ImageFromFileModifier : AbstractModifier
+    {
+        private static readonly string argsInfo = "string | Path";
+
+        public override string Name => "Image from File";
+
+        public override object ApplyTo(object model, params object[] args)
+        {
+            if (args.Length != 1 || args[0] as string is null)
+                throw new ArgumentException("Invalid args.");
+            
+            string path = (string)args[0];
+            try
+            {
+                return (Bitmap)Image.FromFile(path);
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid args.");
+            }
+        }
+
+        public override bool CanBeAppliedTo(Type modelType)
+        {
+            return modelType == null;
+        }
+
+        public override Type ResultType(Type modelType)
+        {
+            return typeof(Bitmap);
+        }
+
+        public override string GetArgsInfo(object model)
+        {
+            return argsInfo;
+        }
+    }
+}
