a6daaa84d9e2159b4d0b4ea078146e90bd284cfe
MainDen <MainDen@DESKTOP-MAIN13>
2021-02-19 17:11:57 +0300

    Restyled of the content of the system buttons.
    Changes:
    The content of the close, minimize and maximize buttons has been changed and bonded to their own buttons.

--- Models Modifier by MainDen/ViewModels/WindowViewModel.cs
+++ Models Modifier by MainDen/ViewModels/WindowViewModel.cs
@@ -10,6 +10,7 @@ namespace Models_Modifier_by_MainDen.ViewModels
         public WindowViewModel(Window window)
         {
             Window = window;
+            Window.SizeChanged += (sender, e) => OnPropertyChanged(nameof(MaximizeSvgContent));
         }
 
         private RelayCommand closeComand;
@@ -24,6 +25,17 @@ namespace Models_Modifier_by_MainDen.ViewModels
                     }));
             }
         }
+        public string CloseSvgContent
+        {
+            get
+            {
+                return "M 0 0 L 10 0 L 10 10 L 0 10 Z"
+                    + "M 1 0 L 5 4 L 9 0 Z"
+                    + "M 1 10 L 5 6 L 9 10 Z"
+                    + "M 0 1 L 4 5 L 0 9 Z"
+                    + "M 10 1 L 6 5 L 10 9 Z";
+            }
+        }
 
         private RelayCommand minimizeCommand;
         public RelayCommand MinimizeCommand
@@ -35,6 +47,13 @@ namespace Models_Modifier_by_MainDen.ViewModels
                     {
                         Window.WindowState = WindowState.Minimized;
                     }));
+            }
+        }
+        public string MinimizeSvgContent
+        {
+            get
+            {
+                return "M 0 1 L 10 1 L 10 2 L 0 2 Z";
             }
         }
 
@@ -64,8 +83,8 @@ namespace Models_Modifier_by_MainDen.ViewModels
             get
             {
                 if (Window.WindowState == WindowState.Maximized)
-                    return "M 0 0 L 0 10 L 10 10 L 10 0 L 2 0 L 2 4 L 8 4 L 8 8 L 2 8 L 2 0 Z";
-                return "M 0 0 L 0 10 L 10 10 L 10 0 L 2 0 L 2 2 L 8 2 L 8 8 L 2 8 L 2 0 Z";
+                    return "M 2 2 L 2 0 L 10 0 L 10 8 L 8 8 L 8 7 L 9 7 L 9 1 L 3 1 L 3 2 Z M 0 2 L 8 2 L 8 10 L 0 10 Z M 1 3 L 7 3 L 7 9 L 1 9 Z";
+                return "M 0 0 L 10 0 L 10 10 L 0 10 Z M 1 1 L 9 1 L 9 9 L 1 9 Z";
             }
         }
     }
